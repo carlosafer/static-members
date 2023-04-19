bc186992ac7135baef09be88d8931c70d2d4956c
carlosafer <carlosafdev@gmail.com>
2023-04-18 23:32:28 -0300

    Non static members changed to static members in class Calculator

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,15 +13,13 @@ public class Program {
 		System.out.print("Enter radius: ");
 		double radius = input.nextDouble();
 		
-		Calculator calculator = new Calculator();
+		double c = Calculator.circumference(radius);
 		
-		double c = calculator.circumference(radius);
-		
-		double v = calculator.volume(radius);
+		double v = Calculator.volume(radius);
 		
 		System.out.printf("Circumference: %.2f%n", c);
 		System.out.printf("Volume: %.2f%n", v);
-		System.out.printf("PI value: %.2f%n", calculator.PI);
+		System.out.printf("PI value: %.2f%n", Calculator.PI);
 		
 		input.close();
 	}
